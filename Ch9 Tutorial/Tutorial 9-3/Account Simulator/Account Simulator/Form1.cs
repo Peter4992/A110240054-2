e6ee19a1877901216d115cf1ff354a236ebee226
Peter4992 <107412765+Peter4992@users.noreply.github.com>
2024-06-03 17:41:26 +0800

    20240603_1741

--- Ch9 Tutorial/Tutorial 9-3/Account Simulator/Account Simulator/Form1.cs
+++ Ch9 Tutorial/Tutorial 9-3/Account Simulator/Account Simulator/Form1.cs
@@ -13,7 +13,7 @@ namespace Account_Simulator
     public partial class Form1 : Form
     {
         // BankAccount field with a $1000 starting balance 
-        
+        private BankAccount account = new BankAccount(1000);
 
         public Form1()
         {
@@ -23,19 +23,37 @@ namespace Account_Simulator
         private void Form1_Load(object sender, EventArgs e)
         {
             // Display the starting balance.
-           
+            balanceLabel.Text = account.Balance.ToString();
         }
 
         private void depositButton_Click(object sender, EventArgs e)
         {
-            
-
-
+            decimal amount;
+            if (decimal.TryParse(depositTextBox.Text, out amount))
+            {
+                account.Deposit(amount);
+                balanceLabel.Text = account.Balance.ToString("c");
+                depositTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("格式錯誤");
+            }
         }
 
         private void withdrawButton_Click(object sender, EventArgs e)
         {
-            
+            decimal amount;
+            if (decimal.TryParse(withdrawTextBox.Text, out amount))
+            {
+                account.Withdraw(amount);
+                balanceLabel.Text = account.Balance.ToString("c");
+                withdrawTextBox.Clear();
+            }
+            else
+            {
+                MessageBox.Show("格式錯誤");
+            }
         }
 
         private void exitButton_Click(object sender, EventArgs e)
